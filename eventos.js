6e1dacb9f6aca3aed62a04eba50e3db861be03f6
unombu <belen.ungari@gmail.com>
2024-09-17 21:38:47 -0300

    solucionado

--- eventos.js
+++ eventos.js
@@ -1,7 +1,12 @@
  //use query selector asi solo me retorna uno y no existe la posibilidad que me retorne más como con getElementsByClassName
 //y asi dsp me deja usar el addEventListener, sino no me deja
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function(){
     document.querySelector("div").addEventListener('click', function() {
         alert("Hola! Soy el div");
     });
-});
+
+    document.getElementById("boton").addEventListener("click", function(e) {
+        e.stopPropagation();
+        alert('Hola!');
+    });
+})
